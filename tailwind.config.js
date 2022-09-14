185b5f1ccc7f8037ee3b632d7ec05810f7202f16
Manion-sebastian <Sjmanion123@gmail.com>
2022-09-13 17:42:13 -0700

    working on adding tailwind. removing structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/src/**/*.{html,js.css}",
-  "./views/*.ejs"],
+  content: ["./views/*.ejs"],
   theme: {
     extend: {},
   },
