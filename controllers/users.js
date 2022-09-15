b50d140bf398586075dd9a8258c8939aaf70a3d8
Manion-sebastian <Sjmanion123@gmail.com>
2022-09-15 10:53:41 -0700

    auth dependancies

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,68 @@
 const express = require('express')
 const router = express.Router()
+// const db = require('../models')
+const crypto = require('crypto-js')
+const bcrypt = require('bcrypt')
 
 router.get('/', (req,res) => {
     res.render('../views/users/home')
+})
+
+router.post('/', async (req,res) => {
+    try {
+        const hashedPassword = bcrypt.hashSync(req.body.password, 12)
+        const [newUser, created] = await db.user.findOrCreate({ where: {
+            email: req.body.email,
+            }, 
+            defaults: {
+                password: hashedPassword
+            }
+        })
+        if (!created) {
+            console.log('user exists')
+            res.redirect('/users/login?message=Please log into your account to continue')
+        } else {
+            const encryptedUserId = crypto.AES.encrypt(newUser.id.toString(), process.env.ENC_SECRET)
+            const encryptedUserIdString = encryptedUserId.toString()
+            res.cookie('userId', encryptedUserIdString)
+            res.redirect('./users/profile')
+
+        }
+
+    } catch(err) {
+        console.warn(err)
+    }
+})
+
+router.post('/login', async (req,res) => {
+    try {
+        const user = await db.user.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        const noLoginMessage = 'Incorrect username or password'
+        if (!user){
+            console.log('user not found') 
+            res.redirect('/users/login?message=' + noLoginMessage)
+        } else if (!bcrypt.compareSync(req.body.password, user.password)) {
+            console.log('wrong password')
+            res.redirect('/users/login?message=' + noLoginMessage)
+        } else {
+            const encryptedUserId = crypto.AES.encrypt(user.id.toString(), process.env.ENC_SECRET)
+            const encryptedUserIdString = encryptedUserId.toString()
+            res.cookie('userId', encryptedUserIdString)
+            res.redirect('/users/profile')
+        }
+    } catch(err) {
+        console.log(err)
+        res.send('server error')
+    }
+})
+
+router.get('/logout', (req,res) => {
+    res.clearCookie('userId')
+    res.redirect('/')
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -4,14 +4,31 @@ const cloudinary = require('cloudinary').v2
 const layout = require('express-ejs-layouts')
 const axios = require('axios')
 const ejs = require('ejs')
+const cookieParser = require('cookie-parser')
+const crypto = require('crypto-js')
 
 const app = express()
 const PORT = 3000
-
 app.set('view engine', 'ejs')
 app.use(layout)
 app.use(express.static(__dirname + '/public/'))
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser())
+
 app.use('/users', require('./controllers/users.js'))
+
+// user auth copied
+// app.use(async (req, res, next) => {
+//     if (req.cookies.userId) {
+//         const decryptedId = crypto.AES.decrypt(req.cookies.userId.toString(), process.env.ENC_SECRET)
+//         const decryptedIdString = decryptedId.toString(crypto.enc.Utf8)
+//         const user = await db.user.findByPk(decryptedIdString)
+//         res.locals.user = user
+//     } else {
+//         res.locals.user = null
+//     }
+//     next()
+// })
 
 app.listen(PORT, () => {
     console.log(`Intruder at ${PORT}`)
