13b4c034ebc707713ebbeaa8496b2d41415d655f
Manion-sebastian <Sjmanion123@gmail.com>
2022-09-15 11:55:06 -0700

    user creation working.

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-// const db = require('../models')
+const db = require('../models')
 const crypto = require('crypto-js')
 const bcrypt = require('bcrypt')
 
@@ -9,60 +9,72 @@ router.get('/', (req,res) => {
 })
 
 router.post('/', async (req,res) => {
-    try {
-        const hashedPassword = bcrypt.hashSync(req.body.password, 12)
-        const [newUser, created] = await db.user.findOrCreate({ where: {
-            email: req.body.email,
-            }, 
-            defaults: {
-                password: hashedPassword
-            }
-        })
-        if (!created) {
-            console.log('user exists')
-            res.redirect('/users/login?message=Please log into your account to continue')
-        } else {
-            const encryptedUserId = crypto.AES.encrypt(newUser.id.toString(), process.env.ENC_SECRET)
-            const encryptedUserIdString = encryptedUserId.toString()
-            res.cookie('userId', encryptedUserIdString)
-            res.redirect('./users/profile')
+    res.send(req.body)
+    await db.user.create({
+        first_name: req.body.fName,
+        last_name: req.body.lName,
+        email: req.body.email,
+        profile_picture: req.body.profile
 
-        }
+    })
 
-    } catch(err) {
-        console.warn(err)
-    }
 })
 
-router.post('/login', async (req,res) => {
-    try {
-        const user = await db.user.findOne({
-            where: {
-                email: req.body.email
-            }
-        })
-        const noLoginMessage = 'Incorrect username or password'
-        if (!user){
-            console.log('user not found') 
-            res.redirect('/users/login?message=' + noLoginMessage)
-        } else if (!bcrypt.compareSync(req.body.password, user.password)) {
-            console.log('wrong password')
-            res.redirect('/users/login?message=' + noLoginMessage)
-        } else {
-            const encryptedUserId = crypto.AES.encrypt(user.id.toString(), process.env.ENC_SECRET)
-            const encryptedUserIdString = encryptedUserId.toString()
-            res.cookie('userId', encryptedUserIdString)
-            res.redirect('/users/profile')
-        }
-    } catch(err) {
-        console.log(err)
-        res.send('server error')
-    }
-})
+// router.post('/', async (req,res) => {
+//     try {
+//         const hashedPassword = bcrypt.hashSync(req.body.password, 12)
+//         const [newUser, created] = await db.user.findOrCreate({ where: {
+//             email: req.body.email,
+//             }, 
+//             defaults: {
+//                 password: hashedPassword
+//             }
+//         })
+//         if (!created) {
+//             console.log('user exists')
+//             res.redirect('/users/login?message=Please log into your account to continue')
+//         } else {
+//             const encryptedUserId = crypto.AES.encrypt(newUser.id.toString(), process.env.ENC_SECRET)
+//             const encryptedUserIdString = encryptedUserId.toString()
+//             res.cookie('userId', encryptedUserIdString)
+//             res.redirect('./users/profile')
 
-router.get('/logout', (req,res) => {
-    res.clearCookie('userId')
-    res.redirect('/')
-})
+//         }
+
+//     } catch(err) {
+//         console.warn(err)
+//     }
+// })
+
+// router.post('/login', async (req,res) => {
+//     try {
+//         const user = await db.user.findOne({
+//             where: {
+//                 email: req.body.email
+//             }
+//         })
+//         const noLoginMessage = 'Incorrect username or password'
+//         if (!user){
+//             console.log('user not found') 
+//             res.redirect('/users/login?message=' + noLoginMessage)
+//         } else if (!bcrypt.compareSync(req.body.password, user.password)) {
+//             console.log('wrong password')
+//             res.redirect('/users/login?message=' + noLoginMessage)
+//         } else {
+//             const encryptedUserId = crypto.AES.encrypt(user.id.toString(), process.env.ENC_SECRET)
+//             const encryptedUserIdString = encryptedUserId.toString()
+//             res.cookie('userId', encryptedUserIdString)
+//             res.redirect('/users/profile')
+//         }
+//     } catch(err) {
+//         console.log(err)
+//         res.send('server error')
+//     }
+// })
+
+// router.get('/logout', (req,res) => {
+//     res.clearCookie('userId')
+//     res.redirect('/')
+// })
 
 module.exports = router
